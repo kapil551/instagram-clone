32363b8650a6bf55def1026228d324cb376171d9
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-11-26 17:31:20 +0530

    Header's Left Section - adding responsive behavior to the image of the left section of header

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,18 @@ function Header() {
                     We need to make the parent div's position as "relative" so that the child image is positiond w.r.t to it's immediate
                     "relatively" positioned parent div.
                 */}
-               <div className="relative w-24 h-24 border-2 border-red-500">
+                {/*
+                    Responsive styling using tailwindcss:
+                    
+                        - Always remember that the default styling that you do using tailwindcss is always for "mobile view".
+                        - We add breakpoints for responsive behavior for larger screens only.
+                    
+                    Adding responsive behavior to the image of the left section of header:
+
+                        - I want to hide the image by defalt for "mobile view" and want to show the image only for width >= 1024px
+                        
+                 */}
+               <div className="relative w-24 h-24 border-2 border-red-500 hidden lg:inline-grid">
 
                     {/* Image */}
                     <Image
